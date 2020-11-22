227bcd250cc25b9507bd0506d84256ab8b757774
yang77 <yang77@qq.com>
2020-11-23 02:02:07 +0800

    feat: 乡情App H5页面

---
+++ src/module/General/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home-page">
+        <div class="wrapper1">General</div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.home-page {
+    .wrapper1 {
+        width: 100px;
+        height: 100px;
+        background: chartreuse;
+    }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,49 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+	{
+		path: '/',
+		redirect: '/home',
+	},
+	{
+		path: '/home',
+		name: 'home-page',
+		component: () =>
+			import(/* webpackChunkName: "home-page" */ '../module/Home/index.vue'),
+	},
+	{
+		path: '/general',
+		name: 'general-page',
+		component: () =>
+			import(
+				/* webpackChunkName: "general-page" */ '../module/General/index.vue'
+			),
+	},
+	{
+		path: '/patients',
+		name: 'patients-page',
+		component: () =>
+			import(
+				/* webpackChunkName: "patients-page" */ '../module/Patients/index.vue'
+			),
+	},
+	{
+		path: '/supervisor',
+		name: 'supervisor-page',
+		component: () =>
+			import(
+				/* webpackChunkName: "supervisor-page" */ '../module/Supervisor/index.vue'
+			),
+	},
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes,
 });
 
 export default router;
