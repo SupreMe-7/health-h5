777d26218e12e0b24e7e25724a34ca51cb00f0f9
yang77 <yang77@qq.com>
2022-01-24 22:38:44 +0800

    feat: 患者可以修改自己的病历

--- src/module/Patients/PersonalCases/index.vue
+++ src/module/Patients/PersonalCases/index.vue
@@ -3,48 +3,108 @@
         <NavBar title="个人病例" />
         <van-tabs v-model="active">
             <van-tab title="健康情况">
-                <van-cell-group title="健康情况">
-                    <van-cell
-                        title="既往疾病史"
-                        :label="healthCondition.jiWangJiBingShi"
-                        size="large"
-                    />
-                    <van-cell
-                        title="服药情况史"
-                        :label="healthCondition.fuYaoQingKuangShi"
-                        size="large"
-                    />
-                    <van-cell
-                        title="外伤及手术史"
-                        :label="healthCondition.waiShangJiShouShuShi"
-                        size="large"
-                    />
-                    <van-cell
-                        title="家族史"
-                        :label="healthCondition.jiaZuShi"
-                        size="large"
-                    />
-                </van-cell-group>
-                <van-cell-group title="生活习惯">
-                    <van-cell
-                        title="嗜烟史"
-                        :label="healthCondition.shiYanShi"
-                        size="large"
-                    />
-                    <van-cell
-                        title="嗜酒史"
-                        :label="healthCondition.shiJiuShi"
-                        size="large"
-                    />
-                    <van-cell
-                        title="生活规律"
-                        :label="healthCondition.shengHuoGuilv"
-                        size="large"
-                    />
-                </van-cell-group>
-                <div class="footer">
-                    <div>录入医生:{{ healthCondition.docName }}</div>
-                    <div>更新时间:{{ healthCondition.updateTime }}</div>
+                <div v-if="!isEdit">
+                    <van-cell-group title="健康情况">
+                        <van-cell
+                            title="既往疾病史"
+                            :label="healthCondition.jiWangJiBingShi"
+                            size="large"
+                        />
+                        <van-cell
+                            title="服药情况史"
+                            :label="healthCondition.fuYaoQingKuangShi"
+                            size="large"
+                        />
+                        <van-cell
+                            title="外伤及手术史"
+                            :label="healthCondition.waiShangJiShouShuShi"
+                            size="large"
+                        />
+                        <van-cell
+                            title="家族史"
+                            :label="healthCondition.jiaZuShi"
+                            size="large"
+                        />
+                    </van-cell-group>
+                    <van-cell-group title="生活习惯">
+                        <van-cell
+                            title="嗜烟史"
+                            :label="healthCondition.shiYanShi"
+                            size="large"
+                        />
+                        <van-cell
+                            title="嗜酒史"
+                            :label="healthCondition.shiJiuShi"
+                            size="large"
+                        />
+                        <van-cell
+                            title="生活规律"
+                            :label="healthCondition.shengHuoGuilv"
+                            size="large"
+                        />
+                    </van-cell-group>
+                    <div class="footer">
+                        <div>录入医生:{{ healthCondition.docName }}</div>
+                        <div>更新时间:{{ healthCondition.updateTime }}</div>
+                        <div class="btn-group">
+                            <van-button type="default" @click="isEdit = true"
+                                >修改</van-button
+                            >
+                        </div>
+                    </div>
+                </div>
+                <div v-else>
+                    <van-cell-group title="健康情况">
+                        <van-field
+                            label="既往疾病史"
+                            v-model="healthCondition.jiWangJiBingShi"
+                            size="large"
+                            placeholder="请输入内容"
+                        />
+                        <van-field
+                            label="服药情况史"
+                            v-model="healthCondition.fuYaoQingKuangShi"
+                            placeholder="请输入内容"
+                            size="large"
+                        />
+                        <van-field
+                            label="外伤及手术史"
+                            v-model="healthCondition.waiShangJiShouShuShi"
+                            placeholder="请输入内容"
+                            size="large"
+                        />
+                        <van-field
+                            label="家族史"
+                            v-model="healthCondition.jiaZuShi"
+                            placeholder="请输入内容"
+                            size="large"
+                        />
+                    </van-cell-group>
+                    <van-cell-group title="生活习惯">
+                        <van-field
+                            label="嗜烟史"
+                            v-model="healthCondition.shiYanShi"
+                            placeholder="请输入内容"
+                            size="large"
+                        />
+                        <van-field
+                            label="嗜酒史"
+                            v-model="healthCondition.shiJiuShi"
+                            placeholder="请输入内容"
+                            size="large"
+                        />
+                        <van-field
+                            label="生活规律"
+                            v-model="healthCondition.shengHuoGuilv"
+                            placeholder="请输入内容"
+                            size="large"
+                        />
+                    </van-cell-group>
+                    <div class="btn-group2">
+                        <van-button type="info" @click="submit"
+                            >提交</van-button
+                        >
+                    </div>
                 </div>
             </van-tab>
             <van-tab title="就诊记录">
@@ -84,6 +144,11 @@
                     <div class="footer">
                         <div>医师:{{ item.docName }}</div>
                         <div>录入时间:{{ item.createTime }}</div>
+                        <div class="btn-group">
+                            <van-button type="default" @click="isEdit = true"
+                                >修改</van-button
+                            >
+                        </div>
                     </div>
                 </div>
             </van-tab>
@@ -93,7 +158,7 @@
 
 <script>
 // 个人病例
-import { Button, Cell, CellGroup, Toast, Tab, Tabs } from 'vant';
+import { Button, Cell, CellGroup, Toast, Tab, Tabs, Field } from 'vant';
 import NavBar from '@/components/NavBar.vue';
 import { getPId } from '@/common/util.js';
 
@@ -102,6 +167,7 @@ export default {
         return {
             active: 0,
             pId: null,
+            isEdit: false,
             diagnosisRecord: [],
             healthCondition: {},
         };
@@ -117,6 +183,7 @@ export default {
         [CellGroup.name]: CellGroup,
         [Tabs.name]: Tabs,
         [Tab.name]: Tab,
+        [Field.name]: Field,
         NavBar,
     },
     methods: {
@@ -127,6 +194,22 @@ export default {
                     const { data } = res;
                     this.healthCondition = data.healthCondition || {};
                     this.diagnosisRecord = data.diagnosisRecord;
+                })
+                .catch(e => {
+                    Toast(e.errMsg);
+                });
+        },
+        submit() {
+            this.$api
+                .post(`/qkys/api/updatePatientHealthCondition`, {
+                    pId: this.pId,
+                    dId: 0,
+                    ...this.healthCondition,
+                })
+                .then(res => {
+                    Toast('修改成功');
+                    this.isEdit = false;
+                    this.getInfo();
                 })
                 .catch(e => {
                     Toast(e.errMsg);
@@ -143,6 +226,10 @@ export default {
         background: #fff;
         padding: 0 10px 0 0;
         margin: 0 0 20px 0;
+        text-align: right;
+    }
+    .btn-group2 {
+        padding: 15px 10px;
         text-align: right;
     }
 }
